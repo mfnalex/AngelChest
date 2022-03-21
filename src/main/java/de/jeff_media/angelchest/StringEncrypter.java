c15ff0e898fd01b17f8c14b49c14caf842bbf670
mfnalex <1122571+mfnalex@users.noreply.github.com>
2022-03-21 14:01:29 +0100

    Improved String Encryption and obfuscation

--- src/main/java/de/jeff_media/angelchest/StringEncrypter.java
+++ src/main/java/de/jeff_media/angelchest/StringEncrypter.java
@@ -5,31 +5,92 @@ import java.util.concurrent.ThreadLocalRandom;
 public class StringEncrypter {
 
     private static int getRandomOffset() {
-        return ThreadLocalRandom.current().nextInt(100,1000);
+        return ThreadLocalRandom.current().nextInt(Character.MIN_VALUE,Character.MAX_VALUE);
+    }
+
+    private static char intToChar(int number) {
+        return (char) number;
+    }
+
+    private static int charToInt(char aChar) {
+        return aChar;
     }
 
     public static String encryptString(String string) {
+        if(true) throw new IllegalStateException();
         int offset = getRandomOffset();
         char[] chars = string.toCharArray();
         for(int i = 0; i < chars.length; i++) {
-            chars[i] = (char) (chars[i] + i*offset);
+            int offset2 = 1;
+            double nan = 0.0d / 0.0;
+            if(i % 4 == 0 && !(nan > nan)) {
+                offset2 = -5;
+            } else if(i % 3 == 0 && !(nan < nan)) {
+                offset2 = 4;
+            } else if(i % 2 == 0 && !(nan == nan)) {
+                offset2 = 3;
+            } else if(nan != nan) {
+                offset2 = -2;
+            }
+
+            chars[i] = (char) (chars[i] + i*offset*offset2);
         }
-        return offset + new String(chars);
+        String result = intToChar(offset) + new String(chars);
+        if(!decryptString(result).equals(string)) {
+            throw new RuntimeException(string);
+        }
+        return result;
     }
 
     public static String decryptString(String string) {
-        int offset = Integer.parseInt(string.substring(0,3));
-        string = string.substring(3);
+        if(true) throw new IllegalStateException();
+        int offset = charToInt(string.toCharArray()[0]);
+        string = string.substring(1);
         char[] chars = string.toCharArray();
+        double nan = 0.0d / 0.0;
         for(int i = 0; i < chars.length; i++) {
-            chars[i] = (char) (chars[i] - i*offset);
+            int offset2 = 1;
+            if(i % 4 == 0 && !(nan > nan)) {
+                offset2 = -5;
+            } else if(i % 3 == 0 && !(nan < nan)) {
+                offset2 = 4;
+            } else if(i % 2 == 0 && !(nan == nan)) {
+                offset2 = 3;
+            } else if(nan != nan) {
+                offset2 = -2;
+            }
+            chars[i] = (char) (chars[i] - i*offset*offset2);
         }
         return new String(chars);
     }
 
+    private enum OffsetResult {
+        MULTIPLY_BY_3(3),
+        MULTIPLY_BY_MINUS_2(-2),
+        MULTIPLY_BY_4(4),
+        MULTIPLY_BY_MINUS_5(5),
+        KEEP(1);
 
-    public static String note() {
-        return "This class is not used in the free version of AngelChest :-)";
+        private final int multiplier;
+
+        OffsetResult(int multiplier) {
+            this.multiplier = multiplier;
+        }
+
+        private static int getOffset(int i) {
+            int offset2 = 1;
+            double nan = 0.0d / 0.0;
+            if(i % 4 == 0 && !(nan > nan)) {
+                offset2 = -5;
+            } else if(i % 3 == 0 && !(nan < nan)) {
+                offset2 = 4;
+            } else if(i % 2 == 0 && !(nan == nan)) {
+                offset2 = 3;
+            } else if(nan != nan) {
+                offset2 = -2;
+            }
+            return offset2;
+        }
     }
 
 }
